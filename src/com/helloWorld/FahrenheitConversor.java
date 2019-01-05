c7abb49809086b2c4a9cb7f63b637c4410d52d1c
Danilo Nascimento <danilo.nascimento.webdesign@gmail.com>
2019-01-05 21:26:56 -0200

    Classe que transforma Graus Celsius em Fahrenheit

---
+++ src/com/helloWorld/FahrenheitConversor.java
@@ -0,0 +1,10 @@
+package com.helloWorld;
+import java.util.Scanner;
+public class FahrenheitConversor {
+    public void grausToFahrenheit(double graus){
+        double farenheit = graus * 9 / 5 + 32;
+        System.out.println(farenheit);
+    }
+
+    ;
+}
--- src/com/helloWorld/Main.java
+++ src/com/helloWorld/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        /**
+         * Escreva um programa que converta uma temperatura digirando em graus celsius
+         * e converta para graus Fahrenheit
+         */
+        FahrenheitConversor fahrenheitConversor = new FahrenheitConversor();
+        fahrenheitConversor.grausToFahrenheit(27);
     }
 }
